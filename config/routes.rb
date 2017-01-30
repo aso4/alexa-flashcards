3536748362675bd3403b2f97e3db127142cb148a
aso4 <albertyso@gmail.com>
2017-01-29 21:39:34 -0500

    add .gitignore, remove sqlite from gemfile for heroku, add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/flashcards', to: 'alexa#dd' #controller name method name
+  post '/', to: 'alexa#index'
 end
