9834d5640781bb8e6f331ec194083cfd3dbb03b5
aso4 <albertyso@gmail.com>
2017-01-29 22:00:15 -0500

    prevent ArgumentError message via commenting

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -1,15 +1,157 @@
-class AlexaController < ApplicationController
-  skip_before_action :verify_authenticity_token
+require 'net/http'
+require 'alexa_skills_ruby'
+require 'CustomHandler'
 
-  def dd
-    handler = Alexa.new(application_id: 'amzn1.ask.skill.8606013b-8292-4e3f-9628-fa472615e78a', logger: Rails.logger)
+class AlexaController < ApplicationController
+  #skip_before_action :verify_authenticity_token
+
+  def index
+
+    #content_type :json
+
+    handler = CustomHandler.new(application_id: 'amzn1.ask.skill.55efad5c-72fc-45bc-aca5-9e713f352e81', logger: logger)
+    #signature = { signature => "signature" }
+
     begin
-      Rails.logger.info request.body.read
       handler.handle(request.body.read)
-      # render json: {bar:'zzz'}
     rescue AlexaSkillsRuby::InvalidApplicationId => e
       logger.error e.to_s
       403
     end
+
   end
+
+  def haze_intent intent
+    result = Net::HTTP.get(URI.parse('http://sghaze.herokuapp.com/'))
+
+    json = JSON.parse(result)
+
+    values = []
+    values << json["North"].to_i
+    values << json["South"].to_i
+    values << json["East"].to_i
+    values << json["West"].to_i
+
+    average = values.sum / values.size.to_f
+
+    description = "hazardous"
+    if average <= 50
+      description = "good"
+    elsif average <= 100
+      description = "moderate"
+    elsif average <= 200
+      description = "unhealthy"
+    elsif average <= 300
+      description = "very unhealthy"
+    end
+
+    @message = "The PSI is now 300"
+  end
+
+  def pregnancy_intent intent
+    date = Date.strptime("14/12/2015", "%d/%m/%Y")
+    week = 40 - (date.cweek - Date.today.cweek)
+    @message = "Baby is now #{week} weeks."
+  end
+
+
+  def lottery_review_intent intent
+    result = Net::HTTP.get(URI.parse('http://gsgresult.appspot.com/api/4d/?afterdrawnum=3800'))
+
+    json = JSON.parse(result)
+
+    slots = intent["slots"]
+    slots.each do |key, value|
+      puts "key:#{key}"
+      if key != "date"
+        next
+      end
+
+      date = Date.strptime(value["value"], "%Y-%m-%d")
+
+      date_string = date.strftime("%b %-d, %Y")
+
+      result = {}
+
+      json.each do |draw|
+        draw_date = draw["drawDate"]
+
+        if draw_date.starts_with?(date_string)
+          result = draw
+          break
+        end
+      end
+
+      @message = "First Prize #{humanize(result["wn1"])};
+      Second Prize #{humanize(result["wn2"])};
+      Third Prize #{humanize(result["wn3"])}"
+    end
+
+    # puts json
+  end
+
+  def lights_intent intent
+    slots = intent["slots"]
+
+    colour = ""
+    on_off = ""
+    slots.each do |key, value|
+      if key == "switch"
+        on_off = value["value"]
+      elsif key == "colour"
+        colour = value["value"]
+      end
+    end
+
+    if colour == "red"
+      if on_off == "on"
+        @@lights |= 0b001
+      else
+        @@lights &= 0b110
+      end
+    elsif colour == "green"
+      if on_off == "on"
+        @@lights |= 0b010
+      else
+        @@lights &= 0b101
+      end
+    elsif colour == "blue"
+      if on_off == "on"
+        @@lights |= 0b100
+      else
+        @@lights &= 0b011
+      end
+    end
+
+    @message = "Please check the lights! #{@@lights}"
+  end
+
+  def humanize number
+    string = []
+    string << (number/1000).to_words
+    string << ((number/100) %10).to_words
+    string << ((number/10) %10).to_words
+    string << (number%10).to_words
+
+    string.join(", ")
+  end
+
+  def bank_account_intent intent
+
+  end
+
 end
+
+#   def dd
+#     handler = Alexa.new(application_id: 'amzn1.ask.skill.55efad5c-72fc-45bc-aca5-9e713f352e81', logger: Rails.logger)
+#     begin
+#       Rails.logger.info request.body.read
+#       handler.handle(request.body.read)
+#       # render json: {bar:'zzz'}
+#     rescue AlexaSkillsRuby::InvalidApplicationId => e
+#       logger.error e.to_s
+#       403
+#     end
+#   end
+#
+# end
