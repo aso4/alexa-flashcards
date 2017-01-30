63fa75aae3c3891d3a23a9011c38d4a685a31779
aso4 <albertyso@gmail.com>
2017-01-30 00:14:58 -0500

    add #favicon to welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def favicon
+    send_file 'assets/images/favicon.ico', type: 'image/x-icon', disposition: 'inline'
+  end
 end
