10c0974dda8830926f5ee89ce533a9c95360f47c
aso4 <albertyso@gmail.com>
2017-01-29 21:44:48 -0500

    add text to index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/flashcards', to: 'alexa#dd' #controller name method name
   post '/', to: 'alexa#index'
+  get '/' do
+    "Hello, this is Alexa Flashcards Ruby."
+  end
 end
