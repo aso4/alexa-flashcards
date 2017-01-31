4883cb79c9b70de26dc95aa34f3b1063705365fe
aso4 <albertyso@gmail.com>
2017-01-31 14:18:38 -0500

    remove invalid object call

--- app/lib/custom_handler.rb
+++ app/lib/custom_handler.rb
@@ -16,7 +16,7 @@ class CustomHandler < AlexaSkillsRuby::Handler
     response.set_simple_card("title", "content")
     logger.info 'StartOverIntent processed'
     logger.info response.class
-    render :json => handler.response
+    render :json => response
   end
 
   on_intent("DailyDataIntent") do
