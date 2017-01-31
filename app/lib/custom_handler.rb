e5687a65dd601790448952820eddfc0ed8c10ce6
aso4 <albertyso@gmail.com>
2017-01-30 22:03:51 -0500

    move alexa.rb intent code to custom_handler

--- app/lib/custom_handler.rb
+++ app/lib/custom_handler.rb
@@ -2,14 +2,41 @@ require 'alexa_skills_ruby'
 
 class CustomHandler < AlexaSkillsRuby::Handler
 
-  on_intent("AMAZON.StartOverIntent") do
+  # on_intent("AMAZON.StartOverIntent") do
+  #   slots = request.intent.slots
+  #   response.set_output_speech_text("Horoscope Text")
+  #   #response.set_output_speech_ssml("<speak><p>Horoscope Text</p><p>More Horoscope text</p></speak>")
+  #   response.set_reprompt_speech_text("Reprompt Horoscope Text")
+  #   #response.set_reprompt_speech_ssml("<speak>Reprompt Horoscope Text</speak>")
+  #   response.set_simple_card("title", "content")
+  #   logger.info 'GetZodiacHoroscopeIntent processed'
+  # end
+
+  on_launch do
+    response.set_output_speech("What's up dog?")
+  end
+
+  on_intent("DailyDataIntent") do
     slots = request.intent.slots
-    response.set_output_speech_text("Horoscope Text")
-    #response.set_output_speech_ssml("<speak><p>Horoscope Text</p><p>More Horoscope text</p></speak>")
-    response.set_reprompt_speech_text("Reprompt Horoscope Text")
-    #response.set_reprompt_speech_ssml("<speak>Reprompt Horoscope Text</speak>")
-    response.set_simple_card("title", "content")
-    logger.info 'GetZodiacHoroscopeIntent processed'
+    response.set_output_speech_text("Something will happen")
+    #response.set_simple_card("title", "content")
+    logger.info 'DailyDataIntent processed'
+  end
+
+  on_intent("StartOverIntent") do
+    response.set_output_speech("What's up dog?")
+  end
+
+  on_intent("AnswerIntent") do
+    @flashcard_answers = Flashcards.new().answers
+    @flashcard_answers.sample
+  end
+
+  on_intent("Intent3") do
+    slots = request.intent.slots
+    response.set_output_speech_text("Something will happen #{slots['attraction']}")
+    #response.set_simple_card("title", "content")
+    logger.info 'DailyDataIntent processed'
   end
 
 end
