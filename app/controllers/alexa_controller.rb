c7c6040741a9cfb3675c6610631760516ef08378
aso4 <albertyso@gmail.com>
2017-02-09 15:59:32 -0500

    add intent responses and debug nil slot error

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -23,13 +23,27 @@ class AlexaController < ApplicationController
     r = AlexaResponse.new
 
     if @echo_request.launch_request?
-      r.spoken_response = "Are you ready for a quiz?"
+      r.spoken_response = "Welcome to Ruby Flashcards. Are you ready to test your knowledge?"
     elsif @echo_request.intent_name == "AMAZON.StartOverIntent"
       @newQuestion = deck.getSample
-      r.spoken_response = "You chose to start over. Here is your next flashcard: #{@newQuestion[2]}.
+      r.spoken_response = "Starting over. Here is your next flashcard: #{@newQuestion[2]}.
       What is the correct answer? #{@newQuestion[3].join(", ")}"
     elsif @echo_request.intent_name == "DontKnowIntent"
-      r.spoken_response = "The answer is #{deck.answers}. Next question."
+      @newQuestion = deck.getSample
+      r.spoken_response = "The answer is #{@newQuestion[4]}. Next question: #{@newQuestion[2]}.
+      What is the correct answer? #{@newQuestion[3].join(", ")}"
+    elsif @echo_request.intent_name == "AnswerIntent" || @echo_request.intent_name == "AnswerOnlyIntent"
+      if true #@echo_request.slots.answer == @newQuestion[1]
+        logger.info "SLOTS ARE: #{@echo_request.slots}"
+        logger.info "STRUCT ANSWER: #{@echo_request.slots.answer}"
+        @newQuestion = deck.getSample
+        r.spoken_response = "That is correct! Next question: #{@newQuestion[2]}.
+        What is the correct answer? #{@newQuestion[3].join(", ")}"
+      else
+        r.spoken_response = "Sorry, that is incorrect. The answer is #{@newQuestion[1]}, #{@newQuestion[4]}.
+        Let's try another question: #{@newQuestion[2]}.
+        What is the correct answer? #{@newQuestion[3].join(", ")}"
+      end
     elsif @echo_request.session_ended_request?
       r.end_session = true
     elsif @echo_request.intent_name == "AMAZON.StopIntent" ||  @echo_request.intent_name == "AMAZON.CancelIntent"
