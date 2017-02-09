d48fcd4fe979945f562ba24d14dcef5fb18810fd
aso4 <albertyso@gmail.com>
2017-02-09 11:43:04 -0500

    modify alexa_response, add alexa_web_service, clean main controller

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -1,81 +1,28 @@
-#require 'net/http'
-#require 'alexa_skills_ruby'
 require 'alexa_web_service'
+require 'httparty'
+require 'json'
 
 class AlexaController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
 
-    # request = AlexaWebService.new(application_id: 'amzn1.ask.skill.8fd7504e-1979-4837-81cc-82db38a26f02', logger: logger)
+    @data = request.body.read
+    params.merge!(JSON.parse(@data))
+    @echo_request = AlexaWebService::AlexaRequest.new(JSON.parse(@data))
+    @application_id = @echo_request.application_id
 
-    # If the request body has been read, like in the Eight Ball example,
-    # you need to rewind it.
+    # If the request body has been read, you need to rewind it.
     request.body.rewind
+    AlexaWebService::AlexaVerify.new(request.env, request.body.read)
 
-    # Verify the request.
-    verified = AlexaWebService::AlexaVerify.new(request.env, request.body.read).verify_request
-    halt 400, "#{verified}" unless verified == "OK"
+    @response = AlexaResponse.new
+    @response.spoken_response = "Hello user"
+    @response.end_session = true
+    logger.info "response is: #{@response.with_card.to_json}"
 
-    begin
+    render json: @response.with_card
 
-      @echo_request = AlexaWebService::AlexaRequest.new(request_json)
-      response = AlexaWebService::Response.new
-
-      if @echo_request.intent_request?
-        response.spoken_response = "Hello user"
-        response.end_session = true
-      end
-
-      response.without_card.to_json
-
-      # the following is for the alexa_skills_ruby gem
-      # hdrs = { 'Signature' => request.env['HTTP_SIGNATURE'], 'SignatureCertChainUrl' => request.env['HTTP_SIGNATURECERTCHAINURL'] }
-      # handler.handle(request.body.read, hdrs)
-      # logger.info handler.response
-      # render handler.response.to_json
-      # handler renders as follows
-      # handler.response.to_json
-
-      # {"shouldEndSession":true,
-      #   "outputSpeech":{
-      #     "type":"PlainText",
-      #     "text":"Start New Deck Text"},
-      #   "card":{
-      #     "type":"Simple",
-      #     "title":"title",
-      #     "content":"content"},
-      #   "reprompt":{
-      #     "outputSpeech":{
-      #       "type":"PlainText",
-      #       "text":"Start New Deck Reprompt Text"}
-      #       }
-      #     }
-
-      # whereas the following code is valid for alexa
-      # resp = {
-      #         "version": "1.0",
-      #         "response": {
-      #           "shouldEndSession": true,
-      #           "outputSpeech": {
-      #             "type": "PlainText",
-      #             "text": "Hello Alexa!"
-      #           },
-      #           "card": {
-      #             "type": "Simple",
-      #             "title": "Greeter",
-      #             "content": "Hello Alexa!"
-      #           }
-      #         }
-      #       }
-      # render :json => resp
-
-      # the request is being read properly via request.body.read
-
-    rescue AlexaSkillsRuby::InvalidApplicationId => e
-      logger.error e.to_s
-      403
-    end
   end
 
   def show
---
+++ app/controllers/alexa_response.rb
@@ -0,0 +1,65 @@
+
+class AlexaResponse
+  attr_accessor :session_attributes, :spoken_response, :ssml_response, :card_title, :card_content, :reprompt_text, :end_session, :speech_type, :text_type
+  def initialize(params = {})
+    @session_attributes = params[:session_attributes] || {}
+    @speech_type = params[:speech_type] || 'PlainText'
+    @spoken_response = params[:spoken_response] || nil
+    @ssml_response = params[:ssml_response] || nil
+    @card_title = params[:card_title] || nil
+    @card_content = params[:card_content] || nil
+    @reprompt_text = params[:reprompt_text] || nil
+    @text_type = params[:text_type] || 'text'
+    @end_session = params[:end_session] || true
+  end
+
+  def add_attribute(key, value)
+    @session_attributes.merge!(key => value)
+  end
+
+  def append_attribute(key, value)
+    @session_attributes[key] << value unless @session_attributes[key].nil?
+  end
+
+  def parse_output_speech(reprompt = false)
+    if !ssml_response
+      {
+        'type' => speech_type,
+        'text' => reprompt ? reprompt_text : spoken_response
+      }
+    else
+      {
+        'type' => 'SSML',
+        'ssml' => ssml_response
+      }
+    end
+  end
+
+  def with_card
+    {
+      'version' => '1.0',
+      'sessionAttributes' =>
+        @session_attributes,
+      'response' => {
+        'shouldEndSession' => end_session,
+        'outputSpeech' => parse_output_speech,
+        'card' => {
+          'type' => 'Simple',
+          'title' => card_title,
+          'content' => card_content
+        },
+        'reprompt' => {
+          'outputSpeech' => parse_output_speech(true)
+        }
+      }
+    }
+  end
+
+  def link_card
+    with_card.tap { |hs| hs['response']['card'] = { 'type' => 'LinkAccount' } }
+  end
+
+  def without_card
+    with_card.tap { |hs| hs['response'].delete('card') }
+  end
+  end
