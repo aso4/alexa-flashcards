702adbb74e5d5ca223f8f68811ea561e0d387f17
aso4 <albertyso@gmail.com>
2017-02-11 00:00:25 -0500

    refactor conditional for AnswerIntent

--- app/controllers/alexa_controller.rb
+++ app/controllers/alexa_controller.rb
@@ -27,37 +27,41 @@ class AlexaController < ApplicationController
     if @echo_request.intent_name == "LaunchIntent"
       #logger.info "ATTRIBUTES ARE: #{@echo_request.application_id} #{@echo_request.user_id} #{@echo_request.access_token} #{@echo_request.session_id}"
       logger.info "onLaunch requestId= #{@echo_request.request_id}, sessionId= #{@echo_request.session_id}"
-      r.add_attribute("speechOutput", "speech output")
-      r.add_attribute("repromptText", "reprompt text")
-      r.add_attribute(  "currentQuestionIndex", 1)
-      r.add_attribute("correctAnswerIndex", 2)
-      r.add_attribute("correctAnswerText", 3)
       r.spoken_response = "Welcome to Ruby Flashcards. Are you ready to test your Ruby knowledge?"
 
     elsif @echo_request.intent_name == "AMAZON.StartOverIntent"
       r.end_session = false
-      # @numResponse = @newQuestion[1].to_i this isn't working
-      logger.info "numResponse is #{@numResponse}"
       r.spoken_response = "Starting over. Here is your next flashcard: #{@newQuestion[2]}.
       What is the correct answer? #{@newQuestion[3].join(", ")}"
+      r.add_attribute("reprompt", "#{@newQuestion[2]}. What is the correct answer? #{@newQuestion[3].join(", ")}")
+      r.add_attribute("currentQuestionIndex", @newQuestion[0])
+      r.add_attribute("correctAnswerIndex", @newQuestion[1])
+      r.add_attribute("correctAnswerText", @newQuestion[4])
 
     elsif @echo_request.intent_name == "DontKnowIntent"
       @newQuestion = deck.getSample
       r.spoken_response = "The answer is #{@newQuestion[4]}. Next question: #{@newQuestion[2]}.
       What is the correct answer? #{@newQuestion[3].join(", ")}"
     elsif @echo_request.intent_name == "AnswerIntent" || @echo_request.intent_name == "AnswerOnlyIntent"
-      if @echo_request.slots.answer && @echo_request.slots.answer.to_i == @newQuestion[1].to_i
-        r.end_session = false
-        logger.info "@newQuestion[1] is #{@newQuestion[1].to_i}"
-        logger.info "STRUCT ANSWER: #{@echo_request.slots.answer.to_i}" #this is your response
-        @newQuestion = deck.getSample
-        r.spoken_response = "That is correct! Next question: #{@newQuestion[2]}.
-        What is the correct answer? #{@newQuestion[3].join(", ")}"
+      if @echo_request.session_attributes?
+        #logger.info "session attributes registered"
+        #logger.info "#{@echo_request.session_attributes?}"
+        logger.info "#{@echo_request.attributes["correctAnswerIndex"]}"
+        logger.info "#{@echo_request.slots.answer.to_i}" #this is the response
+        if @echo_request.attributes["correctAnswerIndex"] == @echo_request.slots.answer.to_i
+          r.end_session = false
+          @newQuestion = deck.getSample
+          r.spoken_response = "That is correct! Next question: #{@newQuestion[2]}.
+          What is the correct answer? #{@newQuestion[3].join(", ")}"
+        else
+          r.end_session = false
+          r.spoken_response = "Sorry, that is incorrect. The answer is #{@newQuestion[1]}, #{@newQuestion[4]}.
+          Let's try another question: #{@newQuestion[2]}.
+          What is the correct answer? #{@newQuestion[3].join(", ")}"
+        end
       else
-        r.end_session = false
-        r.spoken_response = "Sorry, that is incorrect. The answer is #{@newQuestion[1]}, #{@newQuestion[4]}.
-        Let's try another question: #{@newQuestion[2]}.
-        What is the correct answer? #{@newQuestion[3].join(", ")}"
+        logger.info "session data was lost"
+        r.spoken_response "session data was lost"
       end
     elsif @echo_request.session_ended_request?
       r.end_session = true
